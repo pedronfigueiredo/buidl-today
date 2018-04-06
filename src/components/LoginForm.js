f9bc97a3834b154db9379b0718690eddb55c3d64
Pedro Figueiredo <ganseki.figueiredo@gmail.com>
2018-04-06 12:04:08 +1000

    Separate presentational and statefull login components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,189 +1,20 @@
 import React, {Component} from 'react';
 import {Button, Dimmer, Form, Loader, Segment} from 'semantic-ui-react';
 
-import {connect} from 'react-redux';
-import {
-  clearLoginForm,
-  updateLoginForm,
-  userIsNew,
-  userFoundInDb,
-} from '../redux/login.js';
-
-import auth from '../utils/auth.js';
 import 'semantic-ui-css/semantic.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './LoginForm.css';
 
 export class LoginForm extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleLoginFormChange = this.handleLoginFormChange.bind(this);
-    this.handleLoginFormSubmit = this.handleLoginFormSubmit.bind(this);
-    this.handleLoginFormFocus = this.handleLoginFormFocus.bind(this);
-    this.handleLoginFormBlur = this.handleLoginFormBlur.bind(this);
-  }
-
-  componentDidMount() {
-    // Wait for ethereum address from props
-    setTimeout(() => {
-      this.checkIfUserExists();
-    }, 2000);
-  }
-
-  testButton() {
-    console.warn(this.props);
-  }
-
-  getData = async path => {
-    const response = await fetch('/api/' + path, {
-      mode: 'no-cors',
-    });
-
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  postData = async (path, data) => {
-    var strData = JSON.stringify(data);
-    const response = await fetch('/api/' + path, {
-      method: 'post',
-      mode: 'cors',
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      },
-      body: strData,
-    });
-
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleLoginFormChange(e) {
-    const {dispatch} = this.props;
-    const name = e.target.name;
-    const value = e.target.value;
-
-    dispatch(updateLoginForm(name, value));
-  }
-
-  handleLoginFormFocus(e) {
-    e.target.classList.add('active');
-    this.hideInputError(e.target);
-  }
-
-  handleLoginFormBlur(e) {
-    e.target.classList.remove('active');
-    e.target.classList.add('blurred');
-    this.showInputError(e.target);
-  }
-
-  showInputError(input) {
-    const name = input.name;
-    const validity = input.validity;
-    const label = document.getElementById(`${name}Label`).textContent;
-    const error = document.getElementById(`${name}Error`);
-
-    if (!validity.valid) {
-      if (validity.valueMissing) {
-        error.textContent = `${label} is a required field`;
-      } else if (validity.patternMismatch) {
-        error.textContent = `${label} should be a valid email address`;
-      }
-      return false;
-    }
-
-    error.textContent = '';
-    return true;
-  }
-
-  hideInputError(input) {
-    const name = input.name;
-    const error = document.getElementById(`${name}Error`);
-    error.textContent = '';
-  }
-
-  showFormErrors() {
-    const inputs = document.querySelectorAll('input');
-    let isFormValid = true;
-
-    inputs.forEach(input => {
-      input.classList.add('active');
-
-      const isInputValid = this.showInputError(input);
-
-      if (!isInputValid) {
-        isFormValid = false;
-      }
-    });
-    return isFormValid;
-  }
-
-  checkIfUserExists() {
-    const {userAccount, dispatch} = this.props;
-    //this.getData('userexists/' + 'randomaddressthatdoesntexist')
-    this.getData('userexists/' + userAccount)
-      .then(res => {
-        if (res === 'User not found') {
-          dispatch(userIsNew());
-        } else if (res === 'error') {
-          console.err('API Call error');
-          // error screen
-        } else {
-          dispatch(userFoundInDb(res));
-        }
-      })
-      .catch(err => console.err(err));
-  }
-
-  async handleLoginFormSubmit(e) {
-    const {
-      userAccount,
-      loginFormState: {emailAddress, nickname},
-      from,
-    } = this.props;
-
-    const loginDetails = {
-      nickname,
-      emailAddress,
-      address: userAccount,
-    };
-
-    e.preventDefault();
-
-    const user = await this.postData('insertuser', loginDetails);
-    if (user) {
-      this.clearLoginForm();
-      auth.authenticate(() => {
-        this.props.history.push(from);
-      });
-    }
-  }
-
-  clearLoginForm() {
-    const {dispatch} = this.props;
-    const inputFields = document.getElementsByTagName('INPUT');
-    inputFields[1].classList.remove('active');
-    inputFields[1].classList.remove('blurred');
-    this.hideInputError(inputFields[1]);
-    inputFields[2].classList.remove('active');
-    inputFields[2].classList.remove('blurred');
-    this.hideInputError(inputFields[2]);
-    dispatch(clearLoginForm());
-  }
-
   render() {
     const {
-      userAccount,
       isLoading,
+      userAccount,
       loginFormState: {emailAddress, nickname},
+      handleLoginFormChange,
+      handleLoginFormSubmit,
+      handleLoginFormFocus,
+      handleLoginFormBlur,
     } = this.props;
 
     const loader = (
@@ -197,7 +28,7 @@ export class LoginForm extends Component {
     const emailRegex = '[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}';
 
     const loginForm = (
-      <Form className="login-form" onSubmit={this.handleLoginFormSubmit}>
+      <Form className="login-form" onSubmit={handleLoginFormSubmit}>
         <h2 className="login--heading">Welcome to Buidl.Today</h2>
         <p className="login--description">
           To get started, please enter your email address and a nickname.
@@ -220,9 +51,9 @@ export class LoginForm extends Component {
             placeholder="example@buidl.today"
             value={emailAddress}
             pattern={emailRegex}
-            onChange={this.handleLoginFormChange}
-            onFocus={this.handleLoginFormFocus}
-            onBlur={this.handleLoginFormBlur}
+            onChange={handleLoginFormChange}
+            onFocus={handleLoginFormFocus}
+            onBlur={handleLoginFormBlur}
             required
           />
           <p className="form-error" id="emailAddressError" />
@@ -234,9 +65,9 @@ export class LoginForm extends Component {
             name="nickname"
             placeholder="Pedro"
             value={nickname}
-            onChange={this.handleLoginFormChange}
-            onFocus={this.handleLoginFormFocus}
-            onBlur={this.handleLoginFormBlur}
+            onChange={handleLoginFormChange}
+            onFocus={handleLoginFormFocus}
+            onBlur={handleLoginFormBlur}
             required
           />
           <p className="form-error" id="nicknameError" />
@@ -249,4 +80,4 @@ export class LoginForm extends Component {
   }
 }
 
-export default connect()(LoginForm);
+export default LoginForm;
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,7 +2,16 @@ import React, {Component} from 'react';
 import LoginForm from '../components/LoginForm.js';
 
 import {connect} from 'react-redux';
-import {storeWeb3, storeUserAccount} from '../redux/login.js';
+import {
+  updateLoginForm,
+  storeWeb3,
+  storeUserAccount,
+  clearLoginForm,
+  userIsNew,
+  userFoundInDb,
+} from '../redux/login.js';
+
+import auth from '../utils/auth.js';
 
 import BuidlContract from '../contracts/Buidl.json';
 import contract from 'truffle-contract';
@@ -12,6 +21,11 @@ const Buidl = contract(BuidlContract);
 export class Login extends Component {
   constructor(props) {
     super(props);
+
+    this.handleLoginFormChange = this.handleLoginFormChange.bind(this);
+    this.handleLoginFormSubmit = this.handleLoginFormSubmit.bind(this);
+    this.handleLoginFormFocus = this.handleLoginFormFocus.bind(this);
+    this.handleLoginFormBlur = this.handleLoginFormBlur.bind(this);
 
     this.state = {
       web3: null,
@@ -41,6 +55,13 @@ export class Login extends Component {
     this.getNumberOfAgreements();
   }
 
+  componentDidMount() {
+    // Wait for ethereum address from props
+    setTimeout(() => {
+      this.checkIfUserExists();
+    }, 2000);
+  }
+
   getUserAddress() {
     const {web3} = this.state;
     const {dispatch} = this.props;
@@ -63,31 +84,171 @@ export class Login extends Component {
       });
   }
 
-  render() {
+  checkIfUserExists() {
+    const {userAccount, dispatch} = this.props;
+    this.getData('userexists/' + userAccount)
+      .then(res => {
+        if (res === 'User not found') {
+          dispatch(userIsNew());
+        } else if (res === 'error') {
+          console.err('API Call error');
+          // error screen
+        } else {
+          dispatch(userFoundInDb(res));
+        }
+      })
+      .catch(err => console.err(err));
+  }
+
+  // API Helper Methods
+  getData = async path => {
+    const response = await fetch('/api/' + path, {
+      mode: 'no-cors',
+    });
+
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  postData = async (path, data) => {
+    var strData = JSON.stringify(data);
+    const response = await fetch('/api/' + path, {
+      method: 'post',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: strData,
+    });
+
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  // FORM METHODS
+  handleLoginFormChange(e) {
+    const {dispatch} = this.props;
+    const name = e.target.name;
+    const value = e.target.value;
+
+    dispatch(updateLoginForm(name, value));
+  }
+
+  handleLoginFormFocus(e) {
+    e.target.classList.add('active');
+    this.hideInputError(e.target);
+  }
+
+  handleLoginFormBlur(e) {
+    e.target.classList.remove('active');
+    e.target.classList.add('blurred');
+    this.showInputError(e.target);
+  }
+
+  async handleLoginFormSubmit(e) {
     const {
-      isLoading,
-      userWasRecognized,
       userAccount,
-      loginFormState,
-      web3,
-      history,
+      loginFormState: {emailAddress, nickname},
     } = this.props;
 
     const {from} = this.props.location.state || {from: '/'};
-    console.log('from in login', from);
+
+    const loginDetails = {
+      nickname,
+      emailAddress,
+      address: userAccount,
+    };
+
+    e.preventDefault();
+
+    const user = await this.postData('insertuser', loginDetails);
+    if (user) {
+      this.clearLoginForm();
+      auth.authenticate(() => {
+        this.props.history.push(from);
+      });
+    }
+  }
+
+  // FORM HELPERS
+  showInputError(input) {
+    const name = input.name;
+    const validity = input.validity;
+    const label = document.getElementById(`${name}Label`).textContent;
+    const error = document.getElementById(`${name}Error`);
+
+    if (!validity.valid) {
+      if (validity.valueMissing) {
+        error.textContent = `${label} is a required field`;
+      } else if (validity.patternMismatch) {
+        error.textContent = `${label} should be a valid email address`;
+      }
+      return false;
+    }
+
+    error.textContent = '';
+    return true;
+  }
+
+  hideInputError(input) {
+    const name = input.name;
+    const error = document.getElementById(`${name}Error`);
+    error.textContent = '';
+  }
+
+  showFormErrors() {
+    const inputs = document.querySelectorAll('input');
+    let isFormValid = true;
+
+    inputs.forEach(input => {
+      input.classList.add('active');
+
+      const isInputValid = this.showInputError(input);
+
+      if (!isInputValid) {
+        isFormValid = false;
+      }
+    });
+    return isFormValid;
+  }
+
+  clearLoginForm() {
+    const {dispatch} = this.props;
+    const inputFields = document.getElementsByTagName('INPUT');
+    inputFields[1].classList.remove('active');
+    inputFields[1].classList.remove('blurred');
+    this.hideInputError(inputFields[1]);
+    inputFields[2].classList.remove('active');
+    inputFields[2].classList.remove('blurred');
+    this.hideInputError(inputFields[2]);
+    dispatch(clearLoginForm());
+  }
+
+  render() {
+    const {
+      isLoading,
+      userAccount,
+      loginFormState,
+    } = this.props;
 
     return (
-      <div className="App">
+      <div className="login-container">
         <div className="container">
           <LoginForm
-            web3={web3}
-            numberOfAgreements={this.state.numberOfAgreements}
-            userAccount={userAccount}
             isLoading={isLoading}
-            userWasRecognized={userWasRecognized}
+            userAccount={userAccount}
             loginFormState={loginFormState}
-            history={history}
-            from={from}
+            handleLoginFormChange={this.handleLoginFormChange}
+            handleLoginFormSubmit={this.handleLoginFormSubmit}
+            handleLoginFormFocus={this.handleLoginFormFocus}
+            handleLoginFormBlur={this.handleLoginFormBlur}
           />
         </div>
       </div>
@@ -98,9 +259,8 @@ export class Login extends Component {
 function mapStateToProps(state) {
   return {
     web3: state.login.web3,
-    userAccount: state.login.userAccount,
     isLoading: state.login.isLoading,
-    userWasRecognized: state.login.userWasRecognized,
+    userAccount: state.login.userAccount,
     loginFormState: state.login.loginFormState,
   };
 }
