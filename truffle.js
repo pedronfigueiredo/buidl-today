656baec668f10ad308b9844b4d128b7a7aa63da9
Pedro Figueiredo <ganseki.figueiredo@gmail.com>
2018-04-20 16:20:38 +1000

    [WIP] Implement deployment workflow
    - Implement deployment to GitHub pages
    - Implement deployment to rinkeby network

--- truffle.js
+++ truffle.js
@@ -1,4 +1,25 @@
+let HDWalletProvider = require('truffle-hdwallet-provider');
+
+// Getting mnemonic from file
+var fs = require('fs');
+const mnemonic = fs.readFileSync('./metamask-mnemonic.txt', 'utf8');
+console.log(mnemonic);
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          'https://rinkeby.infura.io/XbD2llj6cDZiQCmQtLg2',
+        );
+      },
+      network_id: 4,
+    },
+  },
 };
