036e40293a1c04769d7d7071487d21aabf7ffbf7
Pedro Figueiredo <ganseki.figueiredo@gmail.com>
2018-05-14 17:07:54 +1000

    Style error page

--- src/containers/Error.js
+++ src/containers/Error.js
@@ -1,17 +1,41 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-import LinkButton from '../components/LinkButton';
+import RedButton from '../components/RedButton.js';
+
 import './Error.css';
 
 export class ErrorContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.goHome = this.goHome.bind(this);
+  }
+
+  goHome() {
+    const {history} = this.props;
+    history.push('/');
+  }
+
   render() {
+    const {history} = this.props;
+    const Navbar = () => (
+      <div className="nav-bar">
+        <div className="heading" onClick={this.goHome}>
+          Buidl.Today
+        </div>
+        <div className="username" />
+      </div>
+    );
+
     return (
       <div className="error-container">
+        <Navbar />
         <div className="container">
-          <h1>Error</h1>
-          <p>Oooops... Something went wrong...</p>
-          <LinkButton to="/">Back</LinkButton>
+          <h1>Ooooops...</h1>
+          <p>Something went wrong...</p>
+          <div className="go-back--wrapper">
+            <RedButton onClick={() => history.push('/')} text={'Home'} />
+          </div>
         </div>
       </div>
     );
